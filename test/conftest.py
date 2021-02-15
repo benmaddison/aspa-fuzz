64ea2aec6520c1bf63d4e5325aed386961816518
Ben Maddison <benm@workonline.africa>
2021-02-15 19:51:37 +0200

    factor params and test cases out of test definitions

---
+++ test/conftest.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2021 Ben Maddison. All rights reserved.
+#
+"""aspa validation test fixtures."""
+
+import os
+
+from aspa import (IPV4, IPV6,
+                  AsPath, AsPathSegment, AS_SEQUENCE,
+                  Aspa, AspaSet,
+                  Draft6Validator, HeitzValidator)
+import aspa.neighbors
+import aspa.state
+
+import pytest
+import yaml
+
+
+class TestCase(object):
+    def __init__(self, as_path, neighbor_as, neighbor_type, expected_state):
+        self.as_path = AsPath(AsPathSegment(segment_type=AS_SEQUENCE,
+                                            values=as_path))
+        self.neighbor_as = neighbor_as
+        self.neighbor_type = getattr(aspa.neighbors, neighbor_type)
+        self.expected_state = getattr(aspa.state, expected_state)
+
+    def __repr__(self):
+        return f"<{self.expected_state} path: '{self.as_path}', " \
+               f"from: {self.neighbor_type}>"
+
+
+def read_test_cases():
+    with open(os.path.join(os.path.dirname(__file__), "test_cases.yml")) as f:
+        data = yaml.safe_load(f)
+    return [TestCase(**case) for case in data]
+
+
+@pytest.fixture(scope="session", params=read_test_cases(), ids=repr)
+def case(request):
+    return request.param
+
+
+@pytest.fixture(scope="session", params=(IPV4, IPV6), ids=("IPv4", "IPv6"))
+def afi(request):
+    return request.param
+
+
+@pytest.fixture(scope="session")
+def aspa_set(afi):
+    with open(os.path.join(os.path.dirname(__file__), "aspa_set.yml")) as f:
+        data = yaml.safe_load(f)
+    return AspaSet(*[Aspa(afi=afi, **aspa) for aspa in data])
+
+
+@pytest.fixture(scope="session",
+                params=(Draft6Validator, HeitzValidator),
+                ids=repr)
+def validator_cls(request):
+    return request.param
--- test/test_validators.py
+++ test/test_validators.py
@@ -2,59 +2,31 @@
 #
 """aspa validation tests."""
 
-from aspa import (IPV4, IPV6,
-                  AsPath, AsPathSegment, AS_SEQUENCE, AS_SET,
-                  Aspa, AspaSet,
-                  Draft6Validator, HeitzValidator,
-                  Customer, IxpRouteServer, IxpRouteServerClient,
-                  Peer, Provider,
-                  Invalid, Valid, Unknown, Unverifiable)
+from aspa import (AsPath, AsPathSegment, AS_SEQUENCE, AS_SET,
+                  Customer, Provider,
+                  Unverifiable)
 
 import pytest
 
 
 class TestValidators(object):
 
-    @pytest.mark.parametrize("afi", (IPV4, IPV6))
-    @pytest.mark.parametrize("validator_cls",
-                             (Draft6Validator, HeitzValidator))
-    @pytest.mark.parametrize("neighbor_type",
-                             (Customer, Provider))
-    def test_unverifible(self, afi, validator_cls, neighbor_type):
-        validator = validator_cls(AspaSet(), local_as=10)
-        as_path = AsPath(AsPathSegment(segment_type=AS_SEQUENCE,
-                                       values=(1, 2)),
-                         AsPathSegment(segment_type=AS_SET,
-                                       values=(3, 4)),
-                         AsPathSegment(segment_type=AS_SEQUENCE,
-                                       values=(5, 6)))
+    @pytest.mark.parametrize("neighbor_type", (Customer, Provider), ids=repr)
+    def test_unverifible(self, afi, aspa_set, validator_cls, neighbor_type):
+        validator = validator_cls(aspa_set=aspa_set, local_as=10)
+        as_path = AsPath(AsPathSegment(segment_type=AS_SEQUENCE, values=(1,)),
+                         AsPathSegment(segment_type=AS_SET, values=(2,)),
+                         AsPathSegment(segment_type=AS_SEQUENCE, values=(3,)))
         state = validator.validate(as_path=as_path,
-                                   neighbor_as=6,
+                                   neighbor_as=3,
                                    afi=afi,
                                    neighbor_type=neighbor_type)
         assert state is Unverifiable
 
-    @pytest.mark.parametrize("afi", (IPV4, IPV6))
-    @pytest.mark.parametrize("validator_cls",
-                             (Draft6Validator, HeitzValidator))
-    @pytest.mark.parametrize("path,neighbor,neighbor_type,expected_state",
-                             (((1, 2, 3, 4), 4, Provider, Unknown),
-                              ((1, 2, 2), 2, Peer, Valid),
-                              ((1, 3), 3, Customer, Invalid),
-                              ((1, 1, 2), 5, IxpRouteServer, Valid),
-                              ((), 0, IxpRouteServerClient, Invalid),
-                              ((), 0, IxpRouteServer, Invalid),
-                              ((1, 3), 5, IxpRouteServer, Invalid)))
-    def test_validator(self, afi, validator_cls, path,
-                       neighbor, neighbor_type, expected_state):
-        aspa_set = AspaSet(Aspa(1, (2,), afi),
-                           Aspa(4, (3,), afi),
-                           Aspa(10, (4,), afi))
-        as_path = AsPath(AsPathSegment(segment_type=AS_SEQUENCE,
-                                       values=path))
+    def test_validator(self, afi, aspa_set, validator_cls, case):
         validator = validator_cls(aspa_set, local_as=10)
-        state = validator.validate(as_path=as_path,
-                                   neighbor_as=neighbor,
+        state = validator.validate(as_path=case.as_path,
+                                   neighbor_as=case.neighbor_as,
                                    afi=afi,
-                                   neighbor_type=neighbor_type)
-        assert state is expected_state
+                                   neighbor_type=case.neighbor_type)
+        assert state is case.expected_state
